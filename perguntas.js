4f9d84875d69c2706c8fce8d45f6d01587618d92
flaviahossel <140623075+flaviahossel@users.noreply.github.com>
2024-10-29 08:49:18 -0300

    29/10

---
+++ perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'História',
+    'Qual o causador da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco.'
+)
+criaCartao(
+    'Geografia',
+    'Qual foi o país europeu pioneiro na implementação da Revolução Industrial?',
+    'Inglaterra'
+)
+criaCartao(
+    'Português',
+    'O que são verbos anômalos?',
+    'Verbos anômalos são verbos irregulares que apresentam conjugação tão diferente a ponto de não haver constância nem mesmo nos radicais. Os verbos “ser” e “ir” são anômalos.'
+)
+criaCartao(
+    'Biologia',
+    'Alguns genes só se expressam quando aparecem em homozigose. Esses genes são denominados:',
+    'Recessivos'
+)
+criaCartao(
+    'Matemática',
+    'Qual o Seno de 30°',
+    '1/2'
+
+)
+criaCartao(
+    'Sociologia',
+    'Segundo Émile Durkheim (1858-1917), as três principais características do Fato Social são:',
+    'a generalidade, a exterioridade e a coercitividade.'
+
+)
+criaCartao(
+    'Química',
+    'Qual a nomenclatura do hidrocarboneto CH3/CH2/CH//CH/CH3?',
+    'Pent_2_eno'
+
+)
+criaCartao(
+    'Empreendedorismo',
+    'O que é um PJ?',
+    'PJ é a sigla para Pessoa Jurídica. O termo é usado para identificar uma empresa, independentemente do seu porte.'
+)
+criaCartao(
+    'Biotecnologia',
+    'O que é Biotecnologia Marrom?',
+    'Responsável pela inovação e gestão de recursos sobre ambientes áridos e desertos'
+)
+criaCartao(
+    'Física',
+    'Qual a primeira lei de Newton?',
+    'Lei da Inércia'
+)
+criaCartao(
+    'Filosofia',
+    'Quem criou o mito da caverna?',
+    'Platão'
+)
+criaCartao(
+    'Entretenimento',
+    'Quais os 3 personagens principais de “ The Vampire Diaries”?',
+    'Elena, Damon e Stefan'
+)
